981383e68a3d16ddb3c4f43319aad7282bf61fe1
SanArGuz <santiago.arcilag@gmail.com>
2018-03-21 17:40:34 -0500

    2.1

--- app/src/main/java/com/martinez/steven/practica_2/LogginActivity.java
+++ app/src/main/java/com/martinez/steven/practica_2/LogginActivity.java
@@ -1,8 +1,10 @@
 package com.martinez.steven.practica_2;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -10,7 +12,16 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LogginActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     EditText eUser, ePassword;
     TextView tRegistro;
@@ -35,39 +46,47 @@ public class LogginActivity extends AppCompatActivity {
             pwd = extras.getString("password");
             email = extras.getString("correo");
         }
+        inicializar();
 
     }
-
-    public void OnClickButton_Loggin(View view) {
-        int id  = view.getId();
-
-        if (id == R.id.bLoggin){
-            if (!ePassword.getText().toString().isEmpty() && !eUser.getText().toString().isEmpty()){
-                euser = eUser.getText().toString();
-                epwd = ePassword.getText().toString();
-                Log.d("Variables", euser + epwd + user + email + pwd);
-                if (euser.equals(user) || euser.equals(email)){
-                    if (epwd.equals(pwd)) {
-                        // CAMBIANDO DEL LOGGIN AL PERFIL TRAS REALIZAR EL LOGGIN
-                        Intent intent = new Intent(LogginActivity.this, PirncipalActivity.class);
-                        intent.putExtra("usuario", user);
-                        intent.putExtra("password", pwd);
-                        intent.putExtra("correo", email);
-                        //startActivityForResult(intent, 0010);
-                        startActivity(intent);
-                        finish();
-                    }
+    private void inicializar() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if(firebaseUser != null){
+                    Log.d("FirebaseUser", "Usuario Logeado: "+firebaseUser.getEmail());
                 }else{
-                    Toast.makeText(LogginActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
-                    eUser.setText("");
-                    ePassword.setText("");
+                    Log.d("FirebaseUser", "No hay usuario logeado ");
                 }
-            }else{
-                Toast.makeText(LogginActivity.this, "Llene los campos", Toast.LENGTH_SHORT).show();
             }
-        }
+        };
+    }
+    public void OnClickButton_Loggin(View view) {
+        inciarsesion(eUser.getText().toString(),ePassword.getText().toString());
     }
 
+    private void inciarsesion(String correo, String contrasena){
+        firebaseAuth.signInWithEmailAndPassword(correo, contrasena).
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            goPrincipalActivity();
+                        }else{
+                            Toast.makeText(LogginActivity.this,"Error al iniciar secion",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void goPrincipalActivity() {
+        Intent i = new Intent(LogginActivity.this, PirncipalActivity.class);
+        startActivity(i);
+        finish();
+    }
     public void OnClickText_Registro(View view) {
         int id = view.getId();
         if (id  == R.id.tRegisto){
