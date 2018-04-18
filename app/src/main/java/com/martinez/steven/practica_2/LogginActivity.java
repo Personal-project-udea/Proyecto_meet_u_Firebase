1eeaab19aa976939c3da4e072bc764898c054c41
SanArGuz <santiago.arcilag@gmail.com>
2018-04-18 11:53:37 -0500

    Version 3.0 Merged branhes in master

--- app/src/main/java/com/martinez/steven/practica_2/LogginActivity.java
+++ app/src/main/java/com/martinez/steven/practica_2/LogginActivity.java
@@ -1,8 +1,13 @@
 package com.martinez.steven.practica_2;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
